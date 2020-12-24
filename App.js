885ea80a0be1e8a421790e22af4a1cdf96e5d222
Harry Seo (Jong Hwan Seo) <quentincavendish18@gmail.com>
2020-12-23 16:16:00 -0800

    donezo

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Food({ fav }) {
+    return <h1>I like {fav}</h1>;
+}
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Food 
+                fav="kimchi" 
+            />
+            <Food fav="samgyeopsal" />
+            <Food fav="ramen" />
+            <Food fav="chukumi" />
+        </div>
+    );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Can only render one thing -> Need to put Potato.js into App.js
+ReactDOM.render(<App />, document.getElementById("potato"));
